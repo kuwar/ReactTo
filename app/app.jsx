df16288140d1494cc867a53287980426b214d4ee
kuwar <kuwarsaurav21@gmail.com>
2017-03-28 23:43:05 +0545

    TodoApp component added

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hasHistory} = require('react-router');
+var TodoApp = require('TodoApp');
 
 // Load foundation-sites
 $(document).foundation();
@@ -9,6 +10,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-	<p>Boilerplate project</p>,
+	<TodoApp/>,
 	document.getElementById('app')
 );
