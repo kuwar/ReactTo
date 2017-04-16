abd00d2de41f28253c1c82d22a0dae496106559f
kuwar <kuwarsaurav21@gmail.com>
2017-04-16 17:53:07 +0545

    refactor todo search to work with redux

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -1,21 +1,25 @@
 var React = require('react');
+var {connect} = require('react-redux');
 
-var TodoSearch = React.createClass({
-  handleSearch: function () {
-    var searchText = this.refs.searchText.value;
-    var showCompleted = this.refs.showCompleted.checked; // true or false
+var actions = require('actions');
 
-    this.props.onSearch(showCompleted, searchText);
-  },
+export var TodoSearch = React.createClass({
+
   render: function () {
+    var {dispatch, showCompleted, searchText} = this.props;
     return (
       <div className="container__header">
         <div>
-          <input type="text" ref="searchText" placeholder="Search todos" onChange={this.handleSearch}/>
+          <input type="text" ref="searchText" placeholder="Search todos" value={searchText} onChange={() => {
+              var searchText = this.refs.searchText.value;
+                dispatch(actions.setSearchText(searchText));
+            }}/>
         </div>
         <div>
           <label>
-            <input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
+            <input type="checkbox" ref="showCompleted" checked={showCompleted} onChange={() => {
+                dispatch(actions.toggleShowCompleted());
+              }}/>
             Show completed todos
           </label>
         </div>
@@ -24,4 +28,11 @@ var TodoSearch = React.createClass({
   }
 });
 
-module.exports = TodoSearch;
+export default connect(
+  (state) => {
+    return {
+      searchText: state.searchText,
+      showCompleted: state.showCompleted
+    };
+  }
+)(TodoSearch);
