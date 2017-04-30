6027f28cc2e67bbe0cb12c94cdfea57a6bc29e74
kuwar <kuwarsaurav21@gmail.com>
2017-04-30 22:34:09 +0545

    move firebase credentials to env file

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyBppmQPkrkVrKOuuwKnv1b6PkF1IO7M-T0",
-    authDomain: "react-todo-a5cca.firebaseapp.com",
-    databaseURL: "https://react-todo-a5cca.firebaseio.com",
-    projectId: "react-todo-a5cca",
-    storageBucket: "react-todo-a5cca.appspot.com",
-    messagingSenderId: "973460424330"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
-  
+
   firebase.initializeApp(config);
 } catch (e) {
 
