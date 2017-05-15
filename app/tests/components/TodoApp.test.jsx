cc89d0ab6e57050f20282d2ca12b1abfeed64cdd
kuwar <kuwarsaurav21@gmail.com>
2017-05-16 04:35:18 +0545

    add action for firebase login and logout

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -6,7 +6,8 @@ var TestUtils = require('react-addons-test-utils');
 var $ = require('jquery');
 
 var configureStore = require('configureStore');
-var TodoApp = require('TodoApp');
+
+import {TodoApp} from 'TodoApp';
 import TodoList from 'TodoList';
 
 describe('TodoApp', () => {
